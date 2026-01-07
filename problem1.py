26cc801fbd8838311a27e95b968424f509952602
gokulsubramanisk-lab <gokulsubramanisk@gmail.com>
2026-01-07 15:46:48 +0530

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -1,12 +1,9 @@
 from collections import deque
-
 queue = deque()
 token = 0
 MAX_SIZE = 100
-
 while True:
     command = input().strip().upper()
-
     if command == "ADD":
         if len(queue) < MAX_SIZE:
             token += 1
@@ -14,19 +11,15 @@ while True:
             print(f"Token Generated: {token}")
         else:
             print("Queue Full")
-
     elif command == "SERVE":
         if queue:
             served = queue.popleft()
             print(f"Serving Token: {served}")
         else:
             print("No Students in Queue")
-
     elif command == "STATUS":
         print(f"Students Waiting: {len(queue)}")
-
     elif command == "EXIT":
         break
-
     else:
         print("Invalid Command")
